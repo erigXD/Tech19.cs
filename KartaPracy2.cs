7e60f3d6c28e8c352396dfc04d2bc5d39a53a733
18EM <114591635+erigXD@users.noreply.github.com>
2022-10-05 09:35:02 +0200

    Create KartaPracy2.cs

---
+++ KartaPracy2.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Zad 1
+
+            /*int a = int.Parse(Console.ReadLine());
+            if (a % 3 == 0)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }*/
+
+            // Zad 2
+
+            /*int a = int.Parse(Console.ReadLine());
+            if (a < 1000 && a > 99 && a%7==0)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }*/
+
+            // Zad 3
+
+            /*int a = int.Parse(Console.ReadLine());
+            if (a >= 18)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }*/
+
+            // Zad 4
+
+            /*int waga = int.Parse(Console.ReadLine());
+            if (waga <= 20)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }*/
+
+            // Zad 5
+            /*int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (c < b && c > a || c > b && c < a)
+            {
+                Console.WriteLine("TAK");
+            }
+            else
+            {
+                Console.WriteLine("NIE");
+            }*/
+
+
+        }
+    }
+}
